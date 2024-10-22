1901bc1844a4addcd7598789f87cdd42e916f48b
hungpidev <hungn27091999@gmail.com>
2024-10-22 09:02:58 +0700

    unit 2

--- src/js/data/musics.js
+++ src/js/data/musics.js
@@ -7,4 +7,12 @@ export const musics = [
     duration: "02:11",
     id: 1,
   },
+  {
+    name: "Unit 2: The Laboratory",
+    singer: "4000 Essential English Words 1",
+    path: "../../../music/Unit 2 The Laboratory.mp3",
+    image: "../../../images/4000-Essential-English-Words-1.jpg",
+    duration: "02:14",
+    id: 2,
+  },
 ];
