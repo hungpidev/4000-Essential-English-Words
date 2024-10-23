986b4ae5beba756ec170bed35e02f44acf7745e8
hungpidev <hungn27091999@gmail.com>
2024-10-23 23:08:13 +0700

    s

--- src/js/app.js
+++ src/js/app.js
@@ -375,7 +375,7 @@ class MusicPlayer {
       playPromise.then((_) => {}).catch((_) => {});
     }
     this.isPlaying = true;
-    this.audio.playbackRate = 0.8;
+    this.audio.playbackRate = 0.85;
     playBtn.innerHTML = pauseIcon;
     volumeControls.style.opacity = 1;
     volumeControls.style.visibility = "visible";
